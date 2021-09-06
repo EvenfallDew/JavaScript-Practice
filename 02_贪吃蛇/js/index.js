7637490e015d6196cacaa74af990bc2d872e76c8
EvenfallDew <40364969+EvenfallDew@users.noreply.github.com>
2021-09-06 23:37:44 +0800

    1

---
+++ 02_贪吃蛇/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let startBtn = document.querySelector("#startBtn");
+let pauseBtn = document.querySelector("#pauseBtn");
+
+startBtn.addEventListener("click", () => {
+	console.log(1);
+});
